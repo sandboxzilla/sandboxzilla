fc82642194beb203b8e53a5a04d59ac8772d5821
sandboxzilla <erol@sandboxzilla.net>
2022-04-06 02:52:27 -0700

    Added comments in prep for publix consumption

--- utils/event_handler.py
+++ utils/event_handler.py
@@ -24,8 +24,18 @@ from typing import Any
 class EventHandler(object):
 
     def __init__(self, **kwargs):
+        """
+        The __init__ function is called when an instance of the EVentHandler is created.
+        Used to initialize the instance.
+
+        The **kwargs is a dictionary that contains the arguments passed to the function.
+        The cb_routines dictionary is used to store the callback routines.
+        """
+        # packet is the token passed to the subscriber callbacks
         self.packet = kwargs
+        # src key holds the event id.  It should be passed in when instantiated
         assert 'src' in self.packet, "src key not defined"
+        # Minimum required keys in the packet dictionary
         if 'dest' not in self.packet:
             self.packet['dest'] = None
         if 'payload' not in self.packet:
@@ -41,6 +51,15 @@ class EventHandler(object):
         return self
 
     def unsubscribe(self, name):
+        """
+        The unsubscribe function is used to remove a callback routine from the list of routines that are called when an event is triggered.
+        The unsubscribe function takes one argument, which is the name of the callback routine to be removed.
+
+        :param name: Subscriber ID.  Used to Delete the subscriber from the list of subscribers.
+        :return: the EventHandler instance.  Allows chaining
+
+        :doc-author: Trelent
+        """
         if name in self.cb_routines:
             del self.cb_routines[name]
         return self
